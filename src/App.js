62be2b6952d97adb5dadd373858508881c629b08
ayan_rafiqi <rafiqiayan@gmail.com>
2021-07-31 17:21:24 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more.</p>
+      <strong>this is ayan rafiqi</strong>
+    </>
+  );
+};
+
+export default App;

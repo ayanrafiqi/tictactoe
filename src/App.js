c0e33d1645e9de4c7abdd42c8dabd64a6f9362aa
ayan_rafiqi <rafiqiayan@gmail.com>
2021-08-01 13:15:32 +0530

    created board and square components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board';
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more.</p>
-      <strong>this is ayan rafiqi</strong>
-    </>
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
   );
 };
 

6e3f5edad53bce3bf1827a740fffd0ed1b0c2566
ayan_rafiqi <rafiqiayan@gmail.com>
2021-08-17 00:37:37 +0530

    styled the entire App

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -4,13 +4,33 @@ const StatusMessage = ({ winner, current }) => {
   const noMovesLeft = current.board.every(el => el !== null);
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && `X  and 0 Tied`}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          {' '}
+          Winner is
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is{' '}
+          <span
+            className={current.isXNext === 'X' ? 'text-green' : 'text-orange'}
+          >
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span ClassName="text-green">X</span>{' '}
+          <span className="text-orange">0</span> Tied
+        </>
+      )}
+    </div>
   );
 };
 
